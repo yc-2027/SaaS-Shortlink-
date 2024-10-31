7fd2589bc63e2e65d990ffaee7b32601170a5c92
Omari <augustuschen00@gmail.com>
2024-10-31 11:44:54 +0900

    feature：短链接分页接口新增创建时间以及时间序列化

---
+++ project/src/main/java/com/nageoffer/project/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,66 @@
+package com.nageoffer.project.dto.resp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 短链接分页返回参数
+ */
+@Data
+public class ShortLinkPageRespDTO {
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUrl;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 有效期类型 0：永久有效 1：自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+9")
+    private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+9")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    private String describe;
+    /**
+     * 网站表示
+     */
+    private String favicon;
+
+}
